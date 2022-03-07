3f9730e19545a6caee1d6a98a21c13b437209584
Ahmed El Daly <am.eldaly@outlook.com>
2022-03-07 14:26:05 +0200

    Added program3test

--- InstructionMemory.v
+++ InstructionMemory.v
@@ -14,7 +14,7 @@ module InstructionMemory #
 
     initial
         begin
-            $readmemh("Ref_MachineCode.txt",ROM) ;
+            $readmemh("Program 2_Machine Code.txt",ROM) ;
         end
 
     assign  OutReadData = ROM [InstrAddress >> 2]   ;
